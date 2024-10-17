9a994a161976bad7191a9856ab3cb0c74e53b6b7
Jackynur <jaki36999@gmail.com>
2024-10-17 23:50:18 +0700

    update

--- Tugas/tugas1/script.js
+++ Tugas/tugas1/script.js
@@ -1,26 +1,14 @@
-function appendToDisplay(value) {
-    const display = document.getElementById('display');
-    display.value += value;
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
 }
 
-function clearDisplay() {
-    const display = document.getElementById('display');
-    display.value = '';
+function clearDisplay(){
+    display.value = "";
 }
 
-function calculate() {
-    const display = document.getElementById('display');
-    let expression = display.value;
-
-    try {
-        // Mengganti simbol ^ dengan Math.pow untuk operasi pangkat
-        expression = expression.replace('^', '**');
-
-        // Eksekusi ekspresi aritmatika
-        const result = eval(expression);
-
-        display.value = result;
-    } catch (error) {
-        display.value = 'Error';
-    }
+function calculate(){
+    display.value = eval(display.value);
 }
