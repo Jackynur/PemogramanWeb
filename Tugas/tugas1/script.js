7da8c83cb598cd278196fe280c032c7318d1cd64
Jackynur <jaki36999@gmail.com>
2024-10-18 17:27:18 +0700

    update

--- Tugas/tugas1/script.js
+++ Tugas/tugas1/script.js
@@ -2,7 +2,11 @@
 const display = document.getElementById("display");
 
 function appendToDisplay(input){
-    display.value += input;
+    if(display.value === "0"){
+        display.value = input;
+    }else{
+        display.value += input;
+    }
 }
 
 function clearDisplay(){
@@ -10,5 +14,11 @@ function clearDisplay(){
 }
 
 function calculate(){
-    display.value = eval(display.value);
+    try{
+        let expression = display.value.replace('^', '**');
+        display.value = eval(expression);
+    }catch(error){
+        display.value = "Error";
+    }
 }
+
